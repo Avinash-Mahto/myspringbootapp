c87b01863f1d63617be9c1a78a2212d57371ec65
Avinash-Mahto <aklinuxworld@gmail.com>
2020-08-08 07:28:45 -0400

    Pushing new Spring boot project

---
+++ src/test/java/com/example/myfirstproject/web/WelcomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.myfirstproject.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeControllerTest {
+
+	@Test
+	void testWelcome() {
+		fail("Not yet implemented");
+	}
+
+}
